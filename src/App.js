fbc19f7156db32c1326b882b02413d3c22d56fbc
moonyellow <moonyellow@126.com>
2021-03-25 11:19:44 +0800

    run eject

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import logo from './logo.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App () {
+  const [ok, setOk] = useState(1)
   const d1 = () => {
-
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(1)
+        resolve()
+      }, 2001)
+    })
   }
-  const d2 = () => {
+    const d2 = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log(2)
+          resolve()
+        }, 2000)
+      })
+    }
+    const d3 = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log(3)
+          resolve()
+        }, 2000)
+      })
+    }
+    const start = async () => {
+      await d1()
+      await d2()
+      await d3()
+      setOk((old) => {return old += 1})
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{ok}
+          </p>
+          <button onClick={start}>start</button>
 
+        </header>
+      </div>
+    )
   }
-  const d3 = () => {
 
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
-
-export default App
+  export default App
